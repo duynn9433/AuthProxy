ac41fb9a471b34bfc6e388df96870a178c1a9263
duynn <duynn9433@gmail.com>
2023-12-14 14:23:47 +0700

    Initial commit

---
+++ src/test/java/com/viettel/vtnet/authproxy/AuthProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettel.vtnet.authproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProxyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
